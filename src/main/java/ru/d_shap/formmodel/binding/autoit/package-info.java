6621dbb90eb5dc9833eb85f61360110466e16f8b
dmitry@DMITRY-DELL.Dlink <dmitry@DMITRY-DELL.Dlink>
2018-02-26 23:22:31 +0300

    Добавил javadoc

---
+++ src/main/java/ru/d_shap/formmodel/binding/autoit/package-info.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model autoit binding is a form model implementation for AutoIt.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model autoit binding.
+//
+// Form model autoit binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model autoit binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Form binding for AutoIt.
+ */
+package ru.d_shap.formmodel.binding.autoit;
