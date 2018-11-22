b32c812706b6a173f881c4b4c687f6b10fb6968c
Dmitry Shapovalov <shenf@mail.ru>
2018-11-22 21:08:55 +0300

    Реализация для связанного элемента

---
+++ src/main/java/ru/d_shap/formmodel/binding/autoit/AutoItBindedElementImpl.java
@@ -0,0 +1,72 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model autoit binding is a form model binding implementation for AutoIt.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model autoit binding.
+//
+// Form model autoit binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model autoit binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.autoit;
+
+import autoitx4java.AutoItX;
+
+/**
+ * The AutoIt binded element implementation.
+ *
+ * @author Dmitry Shapovalov
+ */
+final class AutoItBindedElementImpl implements AutoItBindedElement {
+
+    private final AutoItX _autoItX;
+
+    private final String _windowTitle;
+
+    private final String _windowText;
+
+    private final String _controlId;
+
+    AutoItBindedElementImpl(final AutoItX autoItX, final String windowTitle, final String windowText, final String controlId) {
+        super();
+        _autoItX = autoItX;
+        _windowTitle = windowTitle;
+        _windowText = windowText;
+        _controlId = controlId;
+    }
+
+    @Override
+    public String getWindowTitle() {
+        return _windowTitle;
+    }
+
+    @Override
+    public String getWindowText() {
+        return _windowText;
+    }
+
+    @Override
+    public String getControlId() {
+        return _controlId;
+    }
+
+    @Override
+    public void click() {
+        _autoItX.controlClick(_windowTitle, _windowText, _controlId);
+    }
+
+    @Override
+    public String getText() {
+        return _autoItX.controlGetText(_windowTitle, _windowText, _controlId);
+    }
+
+}
