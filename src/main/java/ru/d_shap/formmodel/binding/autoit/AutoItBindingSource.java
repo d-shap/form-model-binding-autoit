b3c48882fcec3e73e21e2da3f1d08ebf4b2411c4
Dmitry Shapovalov <shenf@mail.ru>
2018-11-23 13:53:41 +0300

    Реализация для источника связывания

---
+++ src/main/java/ru/d_shap/formmodel/binding/autoit/AutoItBindingSource.java
@@ -0,0 +1,54 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model autoit binding is a form model binding implementation for AutoIt.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model autoit binding.
+//
+// Form model autoit binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model autoit binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.autoit;
+
+import ru.d_shap.formmodel.binding.model.BindingSource;
+
+import autoitx4java.AutoItX;
+
+/**
+ * The AutoIt binding source.
+ *
+ * @author Dmitry Shapovalov
+ */
+public interface AutoItBindingSource extends BindingSource {
+
+    /**
+     * Get the AutoIt.
+     *
+     * @return the AutoIt.
+     */
+    AutoItX getAutoIt();
+
+    /**
+     * Get the title of the window to access.
+     *
+     * @return the title of the window to access.
+     */
+    String getWindowTitle();
+
+    /**
+     * Get the text of the window to access.
+     *
+     * @return the text of the window to access.
+     */
+    String getWindowText();
+
+}
---
+++ src/main/java/ru/d_shap/formmodel/binding/autoit/AutoItBindingSourceImpl.java
@@ -0,0 +1,66 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model autoit binding is a form model binding implementation for AutoIt.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model autoit binding.
+//
+// Form model autoit binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model autoit binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.autoit;
+
+import autoitx4java.AutoItX;
+
+/**
+ * The AutoIt binding source implementation.
+ *
+ * @author Dmitry Shapovalov
+ */
+final class AutoItBindingSourceImpl implements AutoItBindingSource {
+
+    private final AutoItX _autoItX;
+
+    private final String _windowTitle;
+
+    private final String _windowText;
+
+    AutoItBindingSourceImpl(final AutoItX autoItX, final String windowTitle) {
+        super();
+        _autoItX = autoItX;
+        _windowTitle = windowTitle;
+        _windowText = "";
+    }
+
+    AutoItBindingSourceImpl(final AutoItX autoItX, final String windowTitle, final String windowText) {
+        super();
+        _autoItX = autoItX;
+        _windowTitle = windowTitle;
+        _windowText = windowText;
+    }
+
+    @Override
+    public AutoItX getAutoIt() {
+        return _autoItX;
+    }
+
+    @Override
+    public String getWindowTitle() {
+        return _windowTitle;
+    }
+
+    @Override
+    public String getWindowText() {
+        return _windowText;
+    }
+
+}
