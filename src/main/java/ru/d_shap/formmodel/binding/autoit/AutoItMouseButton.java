3fd7ce1bb5f7e2557c6eaa89f77f363812e6e1ad
Dmitry Shapovalov <shenf@mail.ru>
2018-11-26 15:15:20 +0300

    Константы для кнопок мыши

---
+++ src/main/java/ru/d_shap/formmodel/binding/autoit/AutoItMouseButton.java
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Form model autoit binding is a form model binding implementation for AutoIt.
+// Copyright (C) 2018 Dmitry Shapovalov.
+//
+// This file is part of form model autoit binding.
+//
+// Form model autoit binding is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Form model autoit binding is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.formmodel.binding.autoit;
+
+/**
+ * The AutoIt mouse button constants.
+ *
+ * @author Dmitry Shapovalov
+ */
+public enum AutoItMouseButton {
+
+    LEFT("left"),
+
+    MIDDLE("middle"),
+
+    RIGHT("right");
+
+    private final String _value;
+
+    AutoItMouseButton(final String value) {
+        _value = value;
+    }
+
+    String getValue() {
+        return _value;
+    }
+
+}
